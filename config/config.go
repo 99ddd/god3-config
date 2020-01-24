a3456bb872a049eeab82a8e898aff076e17465c2
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-24 07:24:34 +0700

    Add Setter and Getter for Language Field

--- config/config.go
+++ config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"github.com/99ddd/god3-core-config/storage"
+	"sync"
+	"sync/atomic"
 )
 
 // Config type is the global config of god3 application. It will be
@@ -22,9 +24,36 @@ type Config struct {
 
 var (
 	globalCfg Config
+	count     uint32
+	lock      sync.Mutex
 )
+
+// Set sets the config.
+func Set(cfg Config) Config {
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	if atomic.LoadUint32(&count) != 0 {
+		panic("can not set config twice")
+	}
+	atomic.StoreUint32(&count, 1)
+
+	cfg.Language = setDefault(cfg.Language, "", "en")
+
+	globalCfg = cfg
+
+	return cfg
+}
 
 // Get gets the config.
 func Get() Config {
 	return globalCfg
+}
+
+func setDefault(value, condition, def string) string {
+	if value == condition {
+		return def
+	}
+	return value
 }
